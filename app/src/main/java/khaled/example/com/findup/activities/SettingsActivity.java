409cab324abe1dfbbe63ff607f343795e9db0908
walid4444 <walid44443@yahoo.com>
2018-07-09 19:00:31 +0200

    add some files

---
+++ app/src/main/java/khaled/example/com/findup/activities/SettingsActivity.java
@@ -0,0 +1,18 @@
+package khaled.example.com.findup.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import khaled.example.com.findup.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+
+    }
+}
