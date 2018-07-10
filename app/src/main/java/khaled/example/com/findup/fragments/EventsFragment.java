ae2c4130d5376d6b02c9c4b920ee000582adb819
khaled <kha5775@gmail.com>
2018-07-10 18:51:30 +0200

    Main Fragment Update

---
+++ app/src/main/java/khaled/example/com/findup/fragments/EventsFragment.java
@@ -0,0 +1,75 @@
+package khaled.example.com.findup.fragments;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import khaled.example.com.findup.R;
+import khaled.example.com.findup.activities.EventDetailsActivity;
+import khaled.example.com.findup.adapters.EventsAdapter;
+import khaled.example.com.findup.adapters.NearMeAdapter;
+import khaled.example.com.findup.adapters.RecyclerTouchListener;
+import khaled.example.com.findup.models.Event;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventsFragment extends Fragment {
+
+
+    public EventsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_events, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        List<Event> events = new ArrayList<>();
+        events.add(new Event("Event", "Event Description","6 May",""));
+        events.add(new Event("Event", "Event Description","6 May",""));
+        events.add(new Event("Event", "Event Description","6 May",""));
+        events.add(new Event("Event", "Event Description","6 May",""));
+        events.add(new Event("Event", "Event Description","6 May",""));
+        bindUI(events);
+    }
+
+    private void bindUI(List<Event> events){
+        RecyclerView recyclerView = getActivity().findViewById(R.id.eventsRecyclerView);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        EventsAdapter adapter = new EventsAdapter(getActivity(), events);
+        recyclerView.setAdapter(adapter);
+        recyclerView.smoothScrollToPosition(0);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, true));
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity()
+                , recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                startActivity(new Intent(getActivity(), EventDetailsActivity.class));
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+    }
+}
