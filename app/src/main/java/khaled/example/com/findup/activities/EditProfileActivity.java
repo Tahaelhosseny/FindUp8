78375a7a48ce2b36f52f15a57391bf70d95bd6e2
Ahmed Eltoukhy <36914548+ahmedeltoukhy323@users.noreply.github.com>
2018-07-19 18:53:28 +0200

    Add files via upload

---
+++ app/src/main/java/khaled/example/com/findup/activities/EditProfileActivity.java
@@ -0,0 +1,53 @@
+package com.android.findup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.santalu.maskedittext.MaskEditText;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    Spinner mobileSpinner;
+    MaskEditText editText_phone;
+    Button btn_editProfileBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        editText_phone=findViewById(R.id.editText_phone);
+        btn_editProfileBack=findViewById(R.id.btn_editProfileBack);
+        btn_editProfileBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        mobileSpinner=findViewById(R.id.mobileSpinner);
+        String[] items = new String[]{"+2", "+966", "+900"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, items);
+        mobileSpinner.setAdapter(adapter);
+        editText_phone.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                mobileSpinner.performClick();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            return super.onKeyDown(keyCode, event);
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/khaled/example/com/findup/activities/NotificationsActivity.java
@@ -0,0 +1,35 @@
+package com.android.findup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    Button btn_notificationsBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+
+        btn_notificationsBack=findViewById(R.id.btn_notificationsBack);
+
+        btn_notificationsBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            return super.onKeyDown(keyCode, event);
+        }
+        return true;
+    }
+}
