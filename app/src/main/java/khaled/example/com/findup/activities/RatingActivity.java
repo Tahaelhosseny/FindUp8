51454318359cfc252649b03bec5ae0b24c467e08
Ahmed Eltoukhy <36914548+ahmedeltoukhy323@users.noreply.github.com>
2018-07-27 16:35:33 +0200

    update

---
+++ app/src/main/java/khaled/example/com/findup/activities/RatingActivity.java
@@ -0,0 +1,30 @@
+package khaled.example.com.findup.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import khaled.example.com.findup.R;
+
+import com.willy.ratingbar.RotationRatingBar;
+
+public class RatingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        final RotationRatingBar ratingBar = findViewById(R.id.ratingBar);
+        Button btn_submit = findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float currentRating = ratingBar.getRating();
+                Toast.makeText(RatingActivity.this, "Current Rating is "+currentRating, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
