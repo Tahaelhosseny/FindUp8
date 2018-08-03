b1b02cae62480c79b0ea6c466c715e026ac7737b
khaled550 <kha5775@gmail.com>
2018-08-03 18:33:06 +0200

    Filter ,Notification ,System messages ,You saved Task

--- app/src/main/java/khaled/example/com/findup/activities/SettingsActivity.java
+++ app/src/main/java/khaled/example/com/findup/activities/SettingsActivity.java
@@ -3,7 +3,6 @@ package khaled.example.com.findup.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -61,7 +60,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 break;
 
             case R.id.btn_notifications:
-                startActivity(new Intent(SettingsActivity.this, NotificationsActivity.class));
+                startActivity(new Intent(SettingsActivity.this, NotificationsSettingsActivity.class));
                 break;
 
             case R.id.btn_currency:
