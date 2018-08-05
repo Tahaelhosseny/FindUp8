225f590422516b33a7aec699c3e984e65f2f0e0e
Ahmed Eltoukhy <36914548+ahmedeltoukhy323@users.noreply.github.com>
2018-08-05 15:37:41 +0200

    Revert "profile"
    This reverts commit 0016036

---
+++ app/src/main/java/khaled/example/com/findup/activities/AddProductTruckActivity.java
@@ -0,0 +1,15 @@
+package khaled.example.com.findup.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import khaled.example.com.findup.R;
+
+public class AddProductTruckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product_truck);
+    }
+}
