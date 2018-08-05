4edacdbe67bea73e0f38f345e06b43ef41617c90
khaled <kha5775@gmail.com>
2018-08-05 18:44:12 +0200

    Bottom Bar

--- app/src/main/java/khaled/example/com/findup/activities/MainActivity.java
+++ app/src/main/java/khaled/example/com/findup/activities/MainActivity.java
@@ -12,6 +12,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.felix.bottomnavygation.BottomNav;
+import com.felix.bottomnavygation.ItemNav;
 
 import khaled.example.com.findup.Helper.UI_Utility;
 import khaled.example.com.findup.R;
@@ -24,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     float actionBarSize;
-    BottomNavigationView bottomNavigationView;
+    //BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +45,50 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
 
-        bottomNavigationView = findViewById(R.id.navigation_bottom);
+        //bottomNavigationView = findViewById(R.id.navigation_bottom);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListner);
+        //bottomNavigationView.setOnNavigationItemSelectedListener(navListner);
 
+
+        final BottomNav bottomNav = findViewById(R.id.navigation_bottom);
+        bottomNav.addItemNav(new ItemNav(this, R.drawable.home_unselected_0_5x, R.drawable.home_sel_1_5x));
+        bottomNav.addItemNav(new ItemNav(this, R.drawable.map_1_5x, R.drawable.map_sel_1_5x));
+        bottomNav.addItemNav(new ItemNav(this, R.drawable.search_1_5x, R.drawable.search_sel_0_5x));
+        bottomNav.addItemNav(new ItemNav(this, R.drawable.category_1_5x, R.drawable.category_sel_1_5x));
+        bottomNav.addItemNav(new ItemNav(this, R.drawable.__1_5x, R.drawable.__1_5x));
+        bottomNav.build();
+
+        BottomNav.OnTabSelectedListener listener = new BottomNav.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                Fragment selectedFragment = new MainFragment();
+                //UI_Utility.BottomNavigationMenu_icons_change(bottomNavigationView.getMenu(),item);
+                switch (position) {
+                    case 0:
+                        ToolbarSwitch(true);
+                        selectedFragment = new MainFragment();
+                        break;
+                    case 1:
+                        ToolbarSwitch(false);
+                        selectedFragment = new MapFragment();
+                        break;
+                    case 2:
+                        ToolbarSwitch(false);
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case 3:
+                        ToolbarSwitch(false);
+                        selectedFragment = new CategoryFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_toolbar_container, selectedFragment).commit();
+            }
+
+            @Override
+            public void onTabLongSelected(int position) {
+            }
+        };
+        bottomNav.setTabSelectedListener(listener);
 
         transaction.replace(R.id.main_toolbar_container, new MainFragment()).commit();
     }
@@ -55,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = new MainFragment();
-                    UI_Utility.BottomNavigationMenu_icons_change(bottomNavigationView.getMenu(),item);
+                    //UI_Utility.BottomNavigationMenu_icons_change(bottomNavigationView.getMenu(),item);
                     switch (item.getItemId()) {
                         case R.id.home:
                             ToolbarSwitch(true);
