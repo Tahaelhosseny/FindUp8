01726f9ee351715f38fa880489c0dc8c87d3053a
walid4444 <walid44443@yahoo.com>
2018-08-09 03:03:02 +0200

    fix tab layouts

--- app/src/main/java/khaled/example/com/findup/fragments/StoreDetailsFragment.java
+++ app/src/main/java/khaled/example/com/findup/fragments/StoreDetailsFragment.java
@@ -1,28 +1,34 @@
 package khaled.example.com.findup.fragments;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import khaled.example.com.findup.R;
+import khaled.example.com.findup.models.TabEntity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StoreDetailsFragment extends Fragment {
 
-    TabLayout tabLayout;
 
     public StoreDetailsFragment() {
         // Required empty public constructor
@@ -36,67 +42,42 @@ public class StoreDetailsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_store_details, container, false);
     }
 
+    CommonTabLayout tabLayout;
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        final ViewPager viewPager = getActivity().findViewById(R.id.storeViewpager);
-        setupViewPager(viewPager);
 
 
+        ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+        mTabEntities.add(new TabEntity("Information"));
+        mTabEntities.add(new TabEntity("Products"));
+        ArrayList<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new StoreInfoFragment());
+        fragmentList.add(new ProductsFragment());
         tabLayout = getActivity().findViewById(R.id.storeTabs);
-        tabLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                tabLayout.setupWithViewPager(viewPager);
-                tabLayout.setSelectedTabIndicatorColor(getActivity().getResources().getColor(R.color.material_color_deep_orange_accent));            }
-        });
+        tabLayout.setTabData(mTabEntities,getActivity(),R.id.fl_change,fragmentList);
+        tabLayout.setIconHeight(0);
+        tabLayout.setIconVisible(false);
+        //Typeface mTypeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/sfcompactdisplay_semibold.ttf");
+       // mTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/sfcompactdisplay_semibold.ttf");
 
-    }
+        tabLayout.getTitleView(0).setTypeface(Typeface.create("sfcompactdisplay_semibold", Typeface.NORMAL));
+        tabLayout.getTitleView(1).setTypeface(Typeface.create("sfcompactdisplay_heavy", Typeface.NORMAL));
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter((getChildFragmentManager()));
-        adapter.addFragment(new StoreInfoFragment(), "Information");
-        adapter.addFragment(new CommentsFragment(), "Products");
-        adapter.addFragment(new ProductsFragment(), "Products");
+        //tabLayout.setupWithViewPager(viewPager);
+        //tabLayout.setSelectedTabIndicatorColor(getActivity().getResources().getColor(R.color.material_color_deep_orange_accent));
+        /*tabLayout.clearFocus();
+        tabLayout.setFocusableInTouchMode(false);
+        tabLayout.setFocusable(false);
+        tabLayout.dispatchWindowFocusChanged(false);*/
 
-        viewPager.setAdapter(adapter);
-    }
-
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onPause() {
+        super.onPause();
+        tabLayout.getTitleView(0).setTypeface(Typeface.create("sfcompactdisplay_semibold", Typeface.NORMAL));
+        tabLayout.getTitleView(1).setTypeface(Typeface.create("sfcompactdisplay_heavy", Typeface.NORMAL));
 
-        if (tabLayout != null){
-            tabLayout.clearFocus();
-        }
     }
 }
