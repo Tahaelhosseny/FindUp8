267593142bc2a19bd4f696685108053b7df35d87
mostafa4444 <41346417+mostafa4444@users.noreply.github.com>
2018-11-04 19:34:19 +0200

    Notification Classes

--- app/src/main/java/findupproducts/example/com/findup/Helper/FirebaseMessaging/MyFirebaseMessagingService.java
+++ app/src/main/java/findupproducts/example/com/findup/Helper/FirebaseMessaging/MyFirebaseMessagingService.java
@@ -1,53 +1,63 @@
-package findupproducts.example.com.findup.Helper.FirebaseMessaging;
-
-import android.content.Intent;
-import android.util.Log;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-import com.google.gson.JsonObject;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import findupproducts.example.com.findup.UI.activities.SplashScreenActivity;
-
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
-    private static final String TAG = "MyFirebaseMsgService";
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        if(remoteMessage.getData().size() > 1){
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-            try {
-                JSONObject jsonObject = new JSONObject(remoteMessage.getData().toString());
-                sendPushNotification(jsonObject);
-            }catch (Exception e){
-                Log.e("Exception" , e.getMessage());
-            }
-        }
-    }
-
-    private void sendPushNotification(JSONObject jsonObject) {
-        Log.e(TAG , "Notification Json " + jsonObject.toString());
-        try {
-            JSONObject data = jsonObject.getJSONObject("data");
-            String title = data.getString("title");
-            String message = data.getString("message");
-            String imageUrl = data.getString("image");
-            MyNotificationManager myNotificationManager = new MyNotificationManager(getApplicationContext());
-            Intent intent = new Intent(getApplicationContext() , SplashScreenActivity.class);
-            if(imageUrl.equals("null")){
-                //displaying small notification
-                myNotificationManager.showSmallNotification(title, message, intent);
-            }else{
-                //if there is an image
-                //displaying a big notification
-                myNotificationManager.showBigNotification(title, message, imageUrl, intent);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//package findupproducts.example.com.findup.Helper.FirebaseMessaging;
+//import android.content.Intent;
+//import android.util.Log;
+//import com.google.firebase.messaging.FirebaseMessagingService;
+//import com.google.firebase.messaging.RemoteMessage;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//import findupproducts.example.com.findup.UI.activities.SplashScreenActivity;
+//public class MyFirebaseMessagingService extends FirebaseMessagingService {
+//
+//    private static final String TAG = "MyFirebaseMsgService";
+//
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        if (remoteMessage.getData().size() > 0) {
+//            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+//            try {
+//                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+//                sendPushNotification(json);
+//            } catch (Exception e) {
+//                Log.e(TAG, "Exception: " + e.getMessage());
+//            }
+//        }
+//    }
+//
+//    //this method will display the notification
+//    //We are passing the JSONObject that is received from
+//    //firebase cloud messaging
+//    private void sendPushNotification(JSONObject json) {
+//        //optionally we can display the json into log
+//        Log.e(TAG, "Notification JSON " + json.toString());
+//        try {
+//            //getting the json data
+//            JSONObject data = json.getJSONObject("data");
+//
+//            //parsing json data
+//            String title = data.getString("title");
+//            String message = data.getString("message");
+//            String imageUrl = data.getString("image");
+//
+//            //creating MyNotificationManager object
+//            MyNotificationManager mNotificationManager = new MyNotificationManager(getApplicationContext());
+//
+//            //creating an intent for the notification
+//            Intent intent = new Intent(getApplicationContext(), SplashScreenActivity.class);
+//
+//            //if there is no image
+//            if(imageUrl.equals("null")){
+//                //displaying small notification
+//                mNotificationManager.showSmallNotification(title, message, intent);
+//            }else{
+//                //if there is an image
+//                //displaying a big notification
+//                mNotificationManager.showBigNotification(title, message, imageUrl, intent);
+//            }
+//        } catch (JSONException e) {
+//            Log.e(TAG, "Json Exception: " + e.getMessage());
+//        } catch (Exception e) {
+//            Log.e(TAG, "Exception: " + e.getMessage());
+//        }
+//    }
+//
+//}
